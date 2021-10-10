a7dbb587a85345f3aa9076aee10c9c3658479243
Aytugan Özgan <aytgnozgn@gmail.com>
2021-10-10 14:52:40 +0300

    düzenleme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ControlPanelComponent } from './control-panel/control-panel.component';
-import { EvenComponent } from './game-counter/even/even.component';
 import { GameCounterComponent } from './game-counter/game-counter.component';
-import { OddComponent } from './game-counter/odd/odd.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ControlPanelComponent,
-    OddComponent,
-    EvenComponent,
     GameCounterComponent
   ],
   imports: [
